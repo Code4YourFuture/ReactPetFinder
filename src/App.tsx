d1800a8dcefeb7ebeb6f779df3b4a10a350d46b2
jyotibhambri <135484967+jyotibhambri@users.noreply.github.com>
2023-11-11 20:07:24 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/Header';
-import { PetList } from './components/PetList';
+import { Header } from './components/header';
+import { PetList } from './components/petList';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { PetDetails } from './components/PetDetails';
-import { PetAdoptionForm } from './components/PetAdoptionForm';
+import { PetDetails } from './components/petDetails';
+import { PetAdoptionForm } from './components/petAdoptionForm';
 
 function App() {
   return (
